826858ffe7970ebcbe93e1951b4e4cf23fefd8eb
ysun <yumeng.sun0103@gmail.com>
2015-05-31 14:36:36 -0700

    clean up

--- lexer.py
+++ lexer.py
@@ -1,14 +1,9 @@
 class Lexer:
-	def __init__(self, string):
-		self.string = string
-		self.tokens = self.get_tokens()
+    def __init__(self, string):
+        self.string = string
+        self.tokens = self.get_tokens()
 
 
-	def get_tokens(self):
-		tokens = [t for t in self.string.split(' ') if t]
-		return tokens
-
-
-# if __name__ == '__main__':
-# 	lexer = Lexer('1 + 1 + 4 * 4')
-# 	print lexer.tokens
+    def get_tokens(self):
+        tokens = [t for t in self.string.split(' ') if t]
+        return tokens
